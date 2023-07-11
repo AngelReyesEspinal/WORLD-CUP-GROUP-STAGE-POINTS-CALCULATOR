20a989b7652adbccbae3b6640235a807c4dfb461
Angel Reyes <anggelreyes@hotmail.com>
2023-07-11 17:00:07 -0400

    adding table service

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from api import fetch_played_matches
 from constants import API_URL, GROUPS
 from match_service import filter_matches_by_group, sort_teams_by_group
+from table_service import printTable
 
 played_matches = fetch_played_matches(API_URL)
 
 for group_name in GROUPS:
     current_group = filter_matches_by_group(played_matches, f"Group {group_name}")
     sorted_teams = sort_teams_by_group(current_group)
-    print(sorted_teams)
+    printTable(group_name, sorted_teams)
---
+++ table_service.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def printTable (group_name, sorted_teams):
+    team_names = [team["name"] for team in sorted_teams]
+    team_total_games_played = [team["total_wins"] + team["total_losses"] + team["total_drawn"] for team in sorted_teams]
+    team_total_wins = [team["total_wins"] for team in sorted_teams]
+    team_total_drawn = [team["total_drawn"] for team in sorted_teams]
+    team_total_losses = [team["total_losses"] for team in sorted_teams]
+    team_goals_difference = [ f"+{team['goals_difference']}" if team["goals_difference"] > 0 else team["goals_difference"] for team in sorted_teams]
+    team_points_earned = [team["points_earned"] for team in sorted_teams]
+
+    df = pd.DataFrame(
+        {
+            "Country": team_names,
+            "Played": team_total_games_played,
+            "W": team_total_wins,
+            "D": team_total_drawn,
+            "L": team_total_losses,
+            "GD": team_goals_difference,
+            "Pts": team_points_earned,
+        }
+    )
+
+    print(f"----------------- GROUP {group_name} -----------------")
+    print(df)
+    print("-------------------------------------------")
+    print("")
